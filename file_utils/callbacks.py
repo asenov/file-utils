c7e9a9c617eea4e9055bfe8e241bce829023bfe8
M Asenov <m.assenov@gmail.com>
2022-04-30 19:45:02 +0100

    Add json output format

--- file_utils/callbacks.py
+++ file_utils/callbacks.py
@@ -1,10 +1,24 @@
+import json
+
 from tabulate import tabulate
 
 
-class PrintFilesCallback:
+class PrintFiles:
     def get_all(self, data):
         print(
             tabulate(
                 data, headers=["Id", "original file path", "file name", "created on"]
+            )
+        )
+
+
+class PrintJson:
+    def get_all(self, data):
+        print(
+            json.dumps(
+                [
+                    {"id": row[0], "file_path": f"{row[1]}/{row[2]}", "created": row[3]}
+                    for row in data
+                ]
             )
         )
