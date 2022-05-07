a9b2355c76e7d1911454b38924587941d496e04b
M Asenov <m.assenov@gmail.com>
2022-05-07 16:27:05 +0100

    Action file find files

---
+++ find_files.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+from file_utils import AsTable, AsJSON, find_files
+
+
+def main(params):
+    output_type = {"json": AsJSON, "print": AsTable}
+
+    return find_files(params.db, params.file_name, output_type[params.output]())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Search by file name in a local database"
+    )
+    parser.add_argument("db", type=str, help="Local Database path")
+    parser.add_argument(
+        "file_name",
+        type=str,
+        help="Full file name or file prefix. Insensitive to capital letters",
+    )
+    parser.add_argument(
+        "-o", "--output", default="print", choices=["json", "print"], help="Output mode"
+    )
+    args = parser.parse_args()
+
+    sys.stdout.write(main(args))
+    sys.stdout.flush()
